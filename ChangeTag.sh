39f18048f1af27ac1b540ed0d15f4d5b55324582
Avinash A Murugappan <58096840+Avinash-Murugappan@users.noreply.github.com>
2020-03-23 21:06:02 -0400

    Update ChangeTag.sh

--- ChangeTag.sh
+++ ChangeTag.sh
@@ -1,5 +1,5 @@
 #SWE 645 Assignment2, Team members: Avinash, Rashi, Rushil, Mithilaesh.
 
-#it direct to the pod.yml file
+#it adds new tag version for each build made and deployed to pods
 
 sed "s/tagVersion/$1/g" pods.yml > node-app-pod.yml
